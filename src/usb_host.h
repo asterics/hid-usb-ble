2056d4fa0d58582c96a50501f9c6ed0820bd9f3e
Chris Veigl <veigl@technikum-wien.at>
2025-11-17 17:52:04 +0100

    refactored code

--- src/usb_host.h
+++ src/usb_host.h
@@ -2,29 +2,6 @@
 #ifndef USB_HOST_H
 #define USB_HOST_H
 
-#include <stdint.h>
-
-#define OUTPUT_USB_MOUSE_REPORT_DEBUG_MESSAGES
-
-typedef struct {
-    union {
-        struct {
-            uint8_t button1 : 1;
-            uint8_t button2 : 1;
-            uint8_t button3 : 1;
-            uint8_t reserved : 5;
-        };
-        uint8_t val;
-    } buttons;
-    int8_t x_displacement;
-    int8_t y_displacement;
-    int8_t scroll_wheel;
-} __attribute__((packed)) unified_mouseReport_t;
-
-typedef void (*mouse_report_callback_t)(unified_mouseReport_t* mouse_report);
-
-void register_mouse_report_callback(mouse_report_callback_t callback);
-void unbond_all_devices();
 void start_usb_host();
 
 #endif  // USB_HOST_H
